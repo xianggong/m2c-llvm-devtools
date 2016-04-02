40d072c2bc1252fffb4498274d35385d23f396d9
Xiang Gong <xgong@ece.neu.edu>
2016-04-01 22:58:35 -0400

    * Tab view * TODO: subtract report and plot

--- bin/memAnalyzer.py
+++ bin/memAnalyzer.py
@@ -3,33 +3,37 @@
 
 
 import argparse
+import collections
 import pandas as pd
 from bokeh.charts import Bar
 from bokeh.plotting import show, output_file
+from bokeh.models.widgets import Panel, Tabs
 from bokeh.io import gridplot
 from bokeh.charts import defaults
 import memparser as mp
 
 SCREEN_WIDTH = 1560
 
-defaults.width = int(SCREEN_WIDTH * 0.25)
+defaults.width = int(SCREEN_WIDTH * 0.22)
 defaults.height = defaults.width
 
 
 def is_all_zeros(column):
     unique = column.unique()
-    if len(unique) == 1 and 0 in unique:
-        return True
-    else:
-        return False
+    test = len(unique) == 1 and 0 in unique
+    return bool(test)
 
 
 class Report(object):
     """Memory report"""
 
-    def __init__(self, report_file):
-        self.__report_file = report_file
-        self.__dataframe = mp.get_df(report_file)
+    def __init__(self, report_file=None, dataframe=None):
+        self.__dataframe = None
+        if dataframe:
+            self.__dataframe = dataframe
+            return
+        if report_file:
+            self.__dataframe = mp.get_df(report_file)
 
     def get_dataframe(self):
         """ Get associated dataframe """
@@ -275,22 +279,30 @@ class Report(object):
         access_df = module_df[access_cols]
         access_df_count = pd.melt(access_df,
                                   id_vars='ModuleId',
-                                  value_vars=['NoRetryHits', 'NoRetryMisses'],
+                                  value_vars=['NoRetryHits',
+                                              'NoRetryMisses'],
                                   var_name='type',
                                   value_name='count')
-        access_df_misc = pd.melt(access_df,
-                                 id_vars='ModuleId',
-                                 value_vars=['NoRetryReads',
-                                             'NoRetryReadHits',
-                                             'NoRetryReadMisses',
-                                             'NoRetryWrites',
-                                             'NoRetryWriteHits',
-                                             'NoRetryWriteMisses',
-                                             'NoRetryNCWrites',
-                                             'NoRetryNCWriteHits',
-                                             'NoRetryNCWriteMisses'],
-                                 var_name='group',
-                                 value_name='count')
+        access_df_count_read = pd.melt(access_df,
+                                       id_vars='ModuleId',
+                                       value_vars=['NoRetryReadHits',
+                                                   'NoRetryReadMisses'],
+                                       var_name='type',
+                                       value_name='count')
+
+        access_df_count_write = pd.melt(access_df,
+                                        id_vars='ModuleId',
+                                        value_vars=['NoRetryWriteHits',
+                                                    'NoRetryWriteMisses'],
+                                        var_name='type',
+                                        value_name='count')
+
+        access_df_count_ncwrite = pd.melt(access_df,
+                                          id_vars='ModuleId',
+                                          value_vars=['NoRetryNCWriteHits',
+                                                      'NoRetryNCWriteMisses'],
+                                          var_name='type',
+                                          value_name='count')
 
         if not is_all_zeros(access_df['NoRetryHitRatio']):
             bar_ratio = Bar(access_df,
@@ -310,35 +322,46 @@ class Report(object):
                             ylabel='Access count')
             figures_module.append(bar_count)
 
-        if not is_all_zeros(access_df_misc['count']):
-            bar_misc = Bar(access_df_misc,
-                           label='ModuleId',
-                           values='count',
-                           group='group',
-                           legend='top_right',
-                           xlabel=module_name,
-                           ylabel='Count')
-            figures_module.append(bar_misc)
+        access_df_misc = [access_df_count_read,
+                          access_df_count_write,
+                          access_df_count_ncwrite]
+        for dataframe in access_df_misc:
+            if not is_all_zeros(dataframe['count']):
+                bar_count = Bar(dataframe,
+                                values='count',
+                                label='ModuleId',
+                                stack='type',
+                                legend='top_right',
+                                xlabel=module_name,
+                                ylabel='Access count')
+                figures_module.append(bar_count)
 
         return figures_module
 
     def __get_figures_by_module(self, module_name='l1'):
-        figures_module = []
-        figures_module.extend(self.__get_figures_module_access(module_name))
-        figures_module.extend(self.__get_figures_module_reads(module_name))
-        figures_module.extend(self.__get_figures_module_writes(module_name))
-        figures_module.extend(self.__get_figures_module_ncwrites(module_name))
-        figures_module.extend(self.__get_figures_module_noretry(module_name))
+        figures_module = collections.OrderedDict()
+        figures_module['access'] = self.__get_figures_module_access(
+            module_name)
+        figures_module['reads'] = self.__get_figures_module_reads(module_name)
+        figures_module['writes'] = self.__get_figures_module_writes(
+            module_name)
+        figures_module['ncwrites'] = self.__get_figures_module_ncwrites(
+            module_name)
+        figures_module['noretry'] = self.__get_figures_module_noretry(
+            module_name)
 
         return figures_module
 
     def get_info(self):
         """ Get information of the report: figures and dataframe """
         info = {}
-        figures = []
+        figures = collections.OrderedDict()
 
         for module in self.__dataframe['ModuleName'].unique():
-            figures.extend(self.__get_figures_by_module(module))
+            figure_dict = self.__get_figures_by_module(module)
+            for index, value in figure_dict.iteritems():
+                name = module + '_' + index
+                figures[name] = value
 
         info['figures'] = figures
         info['dataframe'] = self.__dataframe
@@ -355,6 +378,8 @@ class Reports(object):
         for report in self.__report_files:
             self.__reports.append(Report(report))
 
+        # Substract dataframe
+
     def plot(self):
 
         output_file_name = 'mem_report'
@@ -362,14 +387,29 @@ class Reports(object):
         output_file_title = 'Mem report'
         output_file(output_file_name, title=output_file_title)
 
-        # Get all figures
-        figures = []
+        # Create figure dict
+        figures = collections.OrderedDict()
         for report in self.__reports:
-            figures.append(report.get_info()['figures'])
+            figure_dict = report.get_info()['figures']
+            for key, value in figure_dict.iteritems():
+                figures[key] = []
 
-        # Plot all figures
-        plot = gridplot(figures)
-        show(plot)
+        # Fill figure dict
+        for report in self.__reports:
+            figure_dict = report.get_info()['figures']
+            for key, value in figure_dict.iteritems():
+                figures[key].append(value)
+
+        # Plot all figures by tabs
+        tabs_list = []
+        for key, value in figures.iteritems():
+            if value[0]:  # Ignore blank
+                plot = gridplot(value)
+                tabs_list.append(Panel(child=plot, title=key))
+
+        tabs = Tabs(tabs=tabs_list)
+
+        show(tabs)
 
 
 def main():
